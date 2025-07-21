3d3b6a280cf7f2771914551c9fec53685630898d
Leonidas Neto <leonidasneto88@gmail.com>
2025-07-21 10:59:43 -0300

    Correção nome do banco de dados

--- database/migrations/2025_07_15_123925_update_canais_tipo.php
+++ database/migrations/2025_07_15_123925_update_canais_tipo.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
    public function up(): void
     {
-        Schema::create('canals', function (Blueprint $table) {
+        Schema::create('canais', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->enum('tipo', [
@@ -28,7 +28,7 @@ return new class extends Migration
     }
     public function down(): void
     {
-        Schema::dropIfExists('canals');
+        Schema::dropIfExists('canais');
     }
 };
 
